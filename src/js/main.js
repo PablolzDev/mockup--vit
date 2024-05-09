fd63d2788a35be2edee7cd6ca0bd07b7e431d6ff
PablolzDev <pabloinprogramacion@gmail.com>
2024-05-09 18:12:42 -0500

    Final

--- src/js/main.js
+++ src/js/main.js
@@ -62,12 +62,32 @@ class="d-flex flex-wrap align-items-center justify-content-center justify-conten
     <li><a class="dropdown-item" href="#">Settings</a></li>
     <li><a class="dropdown-item" href="#">Profile</a></li>
     <li><hr class="dropdown-divider" /></li>
-    <li><a class="dropdown-item" href="#">Sign out</a></li>
   </ul>
 </div>
 </div>
 `
 
+const footer = document.querySelector("footer")
+
+footer.innerHTML = ` <footer>
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 m-4 border-top">
+    <p class="col-md-4 mb-0 text-primary">&copy; 2024 Company, Inc</p>
+
+    <a href="/"
+      class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <svg class="bi me-2" width="40" height="32">
+        <use xlink:href="#bootstrap" />
+      </svg>
+    </a>
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+      <li class="nav-item"><a href="./src/pages/results.html" class="nav-link px-2 text-body-secondary">Features</a></li>
+    </ul>
+  </footer>
+</div>
+</footer>`
 
 
 const data = {
@@ -84,9 +104,21 @@ const data = {
       'rgb(54, 162, 235)',
       'rgb(255, 205, 86)'
     ],
-    hoverOffset: 4
-  }]
-};
+    hoverOffset: 4,
+
+    borderWidth: 1,
+  }],
+  
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+  }
+}
 
 
 const config = {
@@ -105,8 +137,28 @@ const config3 = {
   data: data,
 };
 
+const config4 = {
+  type: 'doughnut',
+  data: data,
+  responsive: true
+};
+
+const config5 = {
+  type: 'doughnut',
+  data: data,
+};
+
+const config6 = {
+  type: 'doughnut',
+  data: data,
+};
+
 const myChart = new Chart(document.getElementById('myChart'), config);
 const myChart2 = new Chart(document.getElementById('chart2'), config2);
 const myChart3 = new Chart(document.getElementById('chart3'), config3);
 
+
+const myChart4 = new Chart(document.getElementById('chart4'), config);
+const myChart5 = new Chart(document.getElementById('chart5'), config2);
+const myChart6 = new Chart(document.getElementById('chart6'), config3);
 
